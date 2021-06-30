af3e62aca7edeee4440801126bd8f3c92e768a7c
BartWesthoff-HRO <bartwesthoff2002@gmail.com>
2021-06-30 22:10:10 +0200

    added base layout
    functions and i/o validator added

--- CDMS.py
+++ CDMS.py
@@ -1,5 +1,150 @@
 # hello everyone
 # Bart Westhoff (0991807) 
 # Niels Krommenhoek
+import re
+
+def newUsername():
+    _username = input("What will be ur username?: ").lower()
+    _firstLetter = _username[0].isalpha()
+    _checkUser = re.search("[a-z0-9_'.]{5,20}", _username)
+    if _username and _firstLetter:
+        print("good")
+    else:
+        print("bad")
+    return _username
+
+def newPassword():
+
+    _password = input("What will be ur password?: ")
+    _checkPW = re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", _password)
+    if _checkPW:
+        print("good")
+    else:
+        print("bad")
+    return  _password
+
+def register():
+    username = newUsername()
+    password = newPassword()
 
 
+def newClient():
+    # mail = input("What is the email?: ")
+    # _validEmail = re.search("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", mail)
+    # if _validEmail:
+    #     print("Pass")
+    # street = input("streetname?: ")
+    # if street.isalpha():
+    #     print("pass")
+    # housenumber = input("house number?: ")
+    # if housenumber.isnumeric():
+    #     print("pass")
+    # zipcode = input("zipcode?: ").upper()
+    # if zipcode[0:3].isnumeric() and zipcode[4:5].isalpha() and len(zipcode) ==6:
+    #     print("pass")
+    listOfCities = ["Rotterdam", "Amsterdam", "Alkmaar", "Maastricht", "Utrecht", "Almere", "Lelystad", "Maassluis", "Vlaardingen", "Schiedam"]
+    index =1
+    while index <= len(listOfCities):
+        print(f"{index}. {listOfCities[index-1]}")
+        index +=1
+    city = listOfCities[(int(input("In wich city do you live (choose from 1-10)")))-1]
+    print(city)
+    mobile_number = input("What is your mobile number?:\n31-6-")
+    if mobile_number.isnumeric() and len(mobile_number) == 8:
+        print("pass")
+    mobile_number = "31-6-" + mobile_number
+    print(mobile_number)
+# newClient()
+
+class User():
+
+    def __init__(self):
+        self.username = newUsername()
+        self.pw = newPassword()
+
+class Advisor(User):
+    def __init__(self):
+        super().__init__()
+
+    def changePassword(self):
+        pass
+    #To update their own password
+
+    def addClient(self):
+        pass
+    # To add a new client to the system
+
+    def modifyClient(self):
+        pass
+    # To modify or update the information of a client in the system
+
+    def searchClient(self):
+        pass
+    # To search and retrieve the information of a client
+
+class SystemAdmin(Advisor):
+    def __init__(self):
+        super().__init__()
+
+    def changePassword(self):
+        pass
+    #To update their own password
+
+    def checkUsers(self):
+        # To check the list of users and their roles
+        pass
+
+    def addAdvisor(self):
+        pass
+        # To define and add a new advisor to the system
+
+    def modifyAdvisor(self):
+        pass
+    # To modify or update an existing advisor’s account and profile
+
+    def deleteAdvisor(self):
+        pass
+    #To delete an existing advisor’s account
+
+    def resetAdvisorPassword(self):
+        pass
+    #To reset an existing advisor’s password (a temporary password)
+
+    def makeBackup(self):
+        pass
+    # To make a backup of the system (clients information and users’ data)
+    def seeLogs(self):
+        pass
+    # To see the logs file of the system
+
+    def deleteClient(self):
+        pass
+    # To delete a client's record from the database (note that an advisor can not delete a record, but can only modify or update a client’s information)
+
+## Superadmin intherit allebei omdat systemadmin en adivsor verschillen in inheritance
+class SuperAdmin(SystemAdmin, Advisor):
+    ## Error overleggen/ laten staan
+    def __init__(self):
+        self.username = "superadmin"
+        self.password = 'Admin!23'
+
+    def AddAdmin(self):
+        pass
+        #To define and add a new admin to the system
+    def modifyAdmin(self):
+        pass
+    # To modify or update an existing admin’s account and profile
+    def deleteAdmin(self):
+        pass
+    # To delete an existing admin’s account
+    def resetAdminPassword(self):
+        pass
+    # To reset an existing admin’s password (a temporary password)
+
+
+
+
+
+
+
+p1 = SuperAdmin()
---
+++ classes/Database.py
@@ -0,0 +1,95 @@
+import sqlite3
+
+class Database:
+    def __init__(self, name=None):
+        self.conn = None
+        self.cursor = None
+        if name:
+            self.open(name)
+
+    def open(self, name):
+        try:
+            self.conn = sqlite3.connect(name)
+            self.cursor = self.conn.cursor()
+        except sqlite3.Error as e:
+            print("Error connecting to database!")
+
+    def close(self):
+        if self.conn:
+            self.conn.commit()
+            self.cursor.close()
+            self.conn.close()
+
+    def commit(self):
+        if self.conn:
+            self.conn.commit()
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self,exc_type,exc_value,traceback):
+        self.close()
+
+    def get(self,table,columns,limit=None,where=1):
+        query = "SELECT {0} from {1} WHERE {2};".format(columns,table,where)
+        self.cursor.execute(query)
+
+        # fetch data
+        rows = self.cursor.fetchall()
+
+        return rows[len(rows)-limit if limit else 0:]
+
+    def getLast(self,table,columns):
+        
+        return self.get(table,columns,limit=1)[0]
+
+    def write(self,table,columns,data):
+        
+        query = "INSERT INTO {0} ({1}) VALUES ({2});".format(table,columns,data)
+
+        self.cursor.execute(query)
+
+
+    def query(self,sql,values=None):
+        if values == None:
+            self.cursor.execute(sql)
+        else:
+            self.cursor.execute(sql, values)
+        # self.conn.commit()
+    
+    # Check if all required tables are avalible
+    def checkMigrations(self):
+        # Books table
+        try:
+            data = self.get('books','id')
+        except:
+            self.query("CREATE TABLE `books` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `author` VARCHAR(256) NOT NULL , `country` VARCHAR(256) NOT NULL , `imageLink` TEXT NOT NULL , `language` VARCHAR(256) NOT NULL , `link` VARCHAR(256) NOT NULL , `pages` INT NOT NULL , `title` VARCHAR(256) NOT NULL , `year` YEAR NOT NULL )")
+        
+        # Credentials table
+        try:
+            data = self.get('credentials', 'id')
+        except:
+            self.query("CREATE TABLE `credentials` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `persons_id` INT NOT NULL , `password` VARCHAR(128) NOT NULL , `is_admin` BOOLEAN DEFAULT(FALSE) )")
+        
+        # Persons table
+        try:
+            data = self.get('persons', 'id')
+        except:
+            self.query("CREATE TABLE `persons` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `gender` VARCHAR(128) NOT NULL , `nameset` VARCHAR(128) NOT NULL , `givenname` VARCHAR(128) NOT NULL , `surname` VARCHAR(128) NOT NULL , `streetaddress` VARCHAR(256) NOT NULL , `zipcode` VARCHAR(10) NOT NULL , `city` VARCHAR(128) NOT NULL , `emailaddress` VARCHAR(256) NOT NULL , `telephonenumber` VARCHAR(128) NOT NULL, `username` VARCHAR(128) NOT NULL );")
+            self.query("INSERT INTO `persons` (`gender`, `nameset`, `givenname`, `surname`, `streetaddress`, `zipcode`, `city`, `emailaddress`, `telephonenumber`, `username`) VALUES ('male', 'dutch', 'admin', 'user', 'adminstreet 5', '2550PK', 'admincity', 'admin@gmail.com', '0653839238', 'admin')")
+            self.commit()
+            lastPerson = self.getLast('persons', 'id')
+            self.query(f"INSERT INTO `credentials` (`persons_id`, `password`, `is_admin`) VALUES ({lastPerson[0]}, 'password', 1)")
+            self.commit()
+        
+        # Catalogs table
+        try:
+            data = self.get('catalogs', 'id')
+        except:
+            self.query("CREATE TABLE `catalogs` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `books_id` INT NOT NULL , `amount` INT NOT NULL)")
+
+        # Catalogs table
+        try:
+            data = self.get('loan_items', 'id')
+        except:
+            self.query("CREATE TABLE `loan_items` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `books_id` INT NOT NULL , `user_id` INT NOT NULL)")
